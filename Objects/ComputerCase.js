b55d9c69c2d1954c4004fe7cb5d728174a1880d6
nathangm70 <nathangm70@gmail.com>
2015-10-19 23:26:50 -0400

    worked on item search functionality
    I worked on the item searchfunctionality got that working and pulled
    data from the returned json data. Now i just need to display this data
    tomorrow

---
+++ Objects/ComputerCase.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Nathan on 2015-10-19.
+ */
+
+module.exports = function() {
+    return{
+        //create our object here
+        title:"",
+        image:""
+    }
+};
--- routes/buildAPooter.js
+++ routes/buildAPooter.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var itemSearchHelper = require('../ItemSearch/AmazonItemSearcher');
+
+var NewCompCase = require('../Objects/ComputerCase');
+
 //do a console log i really dunno what is happening here
 console.log('Building a Pooter');
 /* GET home page. */
@@ -11,7 +15,31 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res){
     console.log('i think we did it boys');
-    router.get();
+    itemSearchHelper.searchForItemNamed(req.body.search, ItemSearchCallback);
 });
+
+function ItemSearchCallback(err, results){
+    console.log(results.ItemSearchResponse.Items[0].Item[0].MediumImage[0].URL[0]);
+    console.log(results.ItemSearchResponse.Items[0].Item[0].ItemAttributes[0].Title[0]);
+
+
+
+    var compCases = [];
+
+    //console.log(results.ItemSearchResponse.Items[0].Item.length);
+
+    for (var i = 0 ; i < results.ItemSearchResponse.Items[0].Item.length; i++){
+
+        var compCase = NewCompCase();
+
+        compCase.image = results.ItemSearchResponse.Items[0].Item[i].MediumImage[0].URL[0];
+        compCase.title = results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title[0];
+        console.log(compCase);
+        compCases.push(compCase, i);
+    }
+
+    console.log(compCases[0]);
+    
+}
 
 module.exports = router;
