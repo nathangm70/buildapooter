4bb22886ff2df0ecc32fd8a7ee2d615cf0d40bdf
Nathangm70 <nathangm70@gmail.com>
2015-11-24 18:44:41 -0500

    Finished up users, starting on user builds
    Finished up the users for now just waiting for database confirmation,
    and i started working on user builds; Created the object

--- Objects/SocketConnector.js
+++ Objects/SocketConnector.js
@@ -5,53 +5,11 @@
 var net = require('net');
 
 module.exports = function() {
-    return{
-        sendUser: function(user) {
-            var finished = false;
+    return {
+        getSocket: function() {
             var socket = new net.Socket();
-            socket.connect(50000, 'localhost', function () {
-                socket.write('1/n');
-                socket.write(JSON.stringify(user) + '/n');
-            });
-            socket.on('data', function () {
-                socket.destroy();
-                finished = true;
-            });
-            var tries = 0;
-            while( !finished ){
-                //for now just retry like 10000 times before giving up
-                tries++;
-                if(giveUp(tries)){
-                    finished = true;
-                }
-            }
-
-
-            //check the response for now just return true
-            return true;
-        },
-        login: function(userInfo) {
-            var socket = new net.Socket;
-            socket.connect(50000, 'localhost', function(){
-                socket.write('2\n');
-                socket.write(JSON.stringify(userInfo) + '\n');
-            });
-            socket.on('data', function(){
-                socket.destroy();
-                return true;
-            });
+            socket.connect(50000, '10.48.8.161');
+            return socket;
         }
     }
 };
-
-
-
-function giveUp(tries) {
-    if (tries > 10000) {
-        return true;
-        console.log('gave up');
-    }
-    else{
-        return false;
-    }
-}
--- routes/createAccount.js
+++ routes/createAccount.js
@@ -2,22 +2,53 @@ var express = require('express');
 var router = express.Router();
 var SocketConnector = require('../Objects/SocketConnector');
 
+var User = require('../Objects/User');
+
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
-    res.render('createAccount', { title: 'Build A Pooter' });
+    res.render('createAccount', { title: 'Build A Pooter' , passFail: false});
 });
 
 router.post('/', function(req, res)
 {
     //here we want to take all the information form it into an object and then send that object off to the server
     //make a thing send the stuff win
-    console.log('it did it');
-    var socketSocket = new SocketConnector();
+    if(req.body.password == req.body.confirmPass) {
+        var socketConnector = new SocketConnector();
+        //create our user object and then put all the values into it
+        var user = new User();
+        user.userName = req.body.userName;
+        user.firstName = req.body.firstName;
+        user.lastName = req.body.lastName;
+        user.email = req.body.email;
+        user.password = req.body.passWord;
 
-    var newUser;
+        var socket = socketConnector.getSocket();
+        socket.write('1\n');
+        socket.write(JSON.stringify(user) + '\n');
+        socket.on('data', function(data){
+            //read some data and then destroy our socket or something like that
+            //the data
+            console.log('we got data lets checkit');
+            console.log('Data: ' + data);
 
-    socketSocket.sendUser();
+            //do a thing where we check if it was added or not then we destroy it and return that
+            if(data == 'success'){
+                //we made an account
+            } else {
+                //we didn't make an account do some other stuff here
+                //have dang send back an account creation failure
+            }
+
+            socket.destroy();
+        });
+    } else {
+        //they goofed up the password post it back
+        //display a passwords dont match message
+        res.render('createAccount', { title: 'Build A Pooter', passFail: true});
+    }
+
 });
 
 module.exports = router;
